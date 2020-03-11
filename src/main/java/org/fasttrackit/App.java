9a85cfbceb39b59d2940a86c33a06381a240d727
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-11 12:39:42 +0200

    Initial commit

---
+++ src/main/java/org/fasttrackit/App.java
@@ -0,0 +1,16 @@
+package org.fasttrackit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Math math = new Math();
+        math.sum(3.4,5.1);
+        math.multiply(8,4);
+
+    }
+}
---
+++ src/main/java/org/fasttrackit/Math.java
@@ -0,0 +1,16 @@
+package org.fasttrackit;
+
+public class Math {
+    public double sum ( double first, double second) {
+        double sum = first + second;
+        System.out.println(first + " + " + second + " = " + sum);
+        return sum;
+    }
+
+    public double multiply ( double first, double second) {
+        double multiply = first * second;
+        System.out.println(first + " * " + second + " = " + multiply);
+        return multiply;
+    }
+
+}
