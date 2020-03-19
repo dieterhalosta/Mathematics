cc9d6353c42b7eadce64efa018678fe4902d5847
Dieter Halosta <dieter.halosta@yahoo.com>
2020-03-19 20:29:48 +0200

    Added method overloading.

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -9,7 +9,7 @@ public class App
     public static void main( String[] args )
     {
         Math math = new Math();
-        math.sum(3.4,5.1);
+        math.sum(3.3,5.2);
         math.multiply(8,4);
 
     }
--- src/main/java/org/fasttrackit/Math.java
+++ src/main/java/org/fasttrackit/Math.java
@@ -1,15 +1,27 @@
 package org.fasttrackit;
 
 public class Math {
+
+    public int sum (int first, int second) {
+        int sum = first + second;
+        System.out.println(first + " + " + second + " = " + sum + "\nInt numbers") ;
+        return sum;
+    }
     public double sum ( double first, double second) {
         double sum = first + second;
-        System.out.println(first + " + " + second + " = " + sum);
+        System.out.println(first + " + " + second + " = " + sum + "\nDouble numbers") ;
         return sum;
     }
 
+    public int multiply (int first, int second) {
+        int multiply = first * second;
+        System.out.println(first + " * " + second + " = " + multiply + "\nInt numbers");
+        return multiply;
+
+    }
     public double multiply ( double first, double second) {
         double multiply = first * second;
-        System.out.println(first + " * " + second + " = " + multiply);
+        System.out.println(first + " * " + second + " = " + multiply + "\nDouble numbers");
         return multiply;
     }
 
